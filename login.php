eb2f3cb8e1e1d2ddf725f9dbb405a98bb1aa6ac1
14BCE0298 <vishal.sharma2014@vit.ac.in>
2017-04-13 16:34:22 +0530

    Add files via upload

---
+++ login.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+
+<form action="login_validate.php" method="post">
+UserId: <input type="text" name="UserId"><br>
+Password: <input type="text" name="Password"><br>
+<input type="submit">
+</form>
+
+</body>
+</html>
